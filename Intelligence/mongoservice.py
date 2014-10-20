0ec7fa0b9dc2ccf94cad15ab46f8f5de8c9e5e35
Bishop-dev <alex.bishop@yandex.ru>
2014-10-20 23:18:29 +0300

    Tracking of user. Need tests

--- Intelligence/mongoservice.py
+++ Intelligence/mongoservice.py
@@ -1,4 +1,30 @@
+import datetime
+import pymongo
+from scrapy.conf import settings
+from time import mktime
+
 class MongoService():
 
+    def __init__(self):
+        connection = pymongo.Connection(settings['MONGODB_SERVER'], settings['MONGODB_PORT'])
+        db = connection[settings['MONGODB_DB']]
+        self.temp_track_collection = db[settings['MONGODB_COLLECTION_TEMP_TRACK']]
+        self.tracking_collection = db[settings['MONGODB_COLLECTION_TRACK']]
+
     def track_status(self, status):
+        dt = datetime.datetime.now()
+        current_time = datetime.datetime(dt.year, dt.month, dt.day, dt.hour, dt.minute)
         print status
+        if status == 'Online':
+            previous = self.temp_track_collection.find_one()
+            if previous is None:
+                self.temp_track_collection.insert({"time": current_time})
+            else:
+                stored_time = previous['time']
+                stored_minutes = self.calculate_minutes(stored_time)
+                if self.calculate_minutes(current_time) - stored_minutes > 5:
+                    self.tracking_collection.insert({"start": stored_time, "end": current_time})
+                    self.temp_track_collection.drop()
+
+    def calculate_minutes(self, timestamp):
+        return mktime(timestamp.timetuple()) / 60
--- Intelligence/settings.py
+++ Intelligence/settings.py
@@ -1,17 +1,10 @@
-# -*- coding: utf-8 -*-
-
-# Scrapy settings for Intelligence project
-#
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#
-
 BOT_NAME = 'Intelligence'
 
 SPIDER_MODULES = ['Intelligence.spiders']
 NEWSPIDER_MODULE = 'Intelligence.spiders'
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'Intelligence (+http://www.yourdomain.com)'
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "intelligence"
+MONGODB_COLLECTION_TEMP_TRACK = "temp_track"
+MONGODB_COLLECTION_TRACK = "track"
